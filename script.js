ac3b17e83d51856ccf27cbeb98b5dfe482c3d5de
Chris Azuaje <christopherazuaje@gmail.com>
2023-07-10 14:52:45 -0400

    Improving the mobile version.

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-const checkbox = document.getElementById("check");
 const body = document.querySelector("body");
-const linkGthb = document.querySelector(".social-gthb");
-const linkTwtr = document.querySelector(".social-twtr");
-const linkLnkdn = document.querySelector(".social-lnkdn");
+const checkbox = document.getElementById("check");
 
 checkbox.addEventListener("change", () => {
   if (checkbox.checked) {
     body.style.backgroundColor = "var(--darkest)";
     body.style.color = "white";
     body.style.transition = "0.2s";
-    linkGthb.style.color = "white";
-    linkTwtr.style.color = "white";
-    linkLnkdn.style.color = "white";
-    linkGthb.style.transition = "0.2s";
-    linkTwtr.style.transition = "0.2s";
-    linkLnkdn.style.transition = "0.2s";
   } else {
     body.style.backgroundColor = "var(--lightest)";
     body.style.color = "black";
     body.style.transition = "0.2s";
-    linkGthb.style.color = "black";
-    linkTwtr.style.color = "black";
-    linkLnkdn.style.color = "black";
-    linkGthb.style.transition = "0.2s";
-    linkTwtr.style.transition = "0.2s";
-    linkLnkdn.style.transition = "0.2s";
   }
 });
+
+function toggleMobileMenu(menu) {
+  menu.classList.toggle("open");
+}
