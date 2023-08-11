b1734a14dc29fafbc8df841fa93d6e6cc0bb46aa
Chris Azuaje <christopherazuaje@gmail.com>
2023-08-11 09:46:53 -0400

    Debugging due to removed btn

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-const styles = "font-size: 1.25em; font-weight: bold; padding: 5px;";
+const styles = 'font-size: 1.25em; font-weight: bold; padding: 5px;';
 
 console.group(
-  `%c👋 Hi, I'm Chris Azuaje and thanks for checking out my portfolio!`,
+  `%cHi, I'm Chris Azuaje and thanks for checking out my portfolio!`,
   styles
 );
 
-console.log("%c🐦Twitter - https://twitter.com/chris_azuaje", styles);
-console.log("%c🐈 Github - https://github.com/chris-azuaje", styles);
+console.log('%c🐈 Github - https://github.com/chris-azuaje', styles);
 console.log(
-  "%c👨‍💼 LinkedIn - https://www.linkedin.com/in/chris-azuaje-73ba46126/",
+  '%c👨‍💼 LinkedIn - https://www.linkedin.com/in/chris-azuaje-73ba46126/',
   styles
 );
-console.log("%c📫 Email - christopherazuaje@gmail.com", styles);
+console.log('%c📫 Email - christopherazuaje@gmail.com', styles);
 console.groupEnd();
 
-const body = document.querySelector("body");
-const checkbox = document.getElementById("check");
-const projectsBtn = document.querySelector(".projects-btn");
+const body = document.querySelector('body');
+const checkbox = document.getElementById('check');
+const projectsBtn = document.querySelector('.projects-btn');
 
-projectsBtn.addEventListener("click", () => {
-  window.open(
-    "https://chris-azuaje.github.io/chris-azuaje-portfolio/",
-    "_blank"
-  );
-});
+// projectsBtn.addEventListener('click', () => {
+//   window.open(
+//     'https://chris-azuaje.github.io/chris-azuaje-portfolio/',
+//     '_blank'
+//   );
+// });
 
-checkbox.addEventListener("change", () => {
+checkbox.addEventListener('change', () => {
   if (checkbox.checked) {
-    body.style.backgroundColor = "var(--darkest)";
-    body.style.color = "white";
-    body.style.transition = "0.2s";
-    projectsBtn.style.border = "3px solid white";
+    body.style.backgroundColor = 'var(--darkest)';
+    body.style.color = 'white';
+    body.style.transition = '0.2s';
+    // projectsBtn.style.border = '3px solid white';
   } else {
-    body.style.backgroundColor = "var(--lightest)";
-    body.style.color = "black";
-    body.style.transition = "0.2s";
-    projectsBtn.style.border = "3px solid black";
+    body.style.backgroundColor = 'var(--lightest)';
+    body.style.color = 'black';
+    body.style.transition = '0.2s';
+    // projectsBtn.style.border = '3px solid black';
   }
 });
 
 function toggleMobileMenu(menu) {
-  menu.classList.toggle("open");
+  menu.classList.toggle('open');
 }
