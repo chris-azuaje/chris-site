02cbdb882f7cae3fce599a0466a8b9c5e20096b1
Chris Azuaje <christopherazuaje@gmail.com>
2024-09-12 15:33:46 -0400

    Updates

--- script.js
+++ script.js
@@ -7,32 +7,50 @@ const navIcon = document.getElementById('nav__hamburger-icon');
 const checkbox = document.getElementById('nav__checkbox');
 const projectsBtn = document.querySelector('.projects-btn');
 
-checkbox.addEventListener('change', () => {
-  if (checkbox.checked) {
-    keepBlack();
-    body.style.backgroundColor = 'var(--darkest)';
-    body.style.color = 'white';
-    body.style.transition = '0.2s';
-  } else {
-    body.style.backgroundColor = 'var(--lightest)';
-    body.style.color = 'black';
-    body.style.transition = '0.2s';
+// Check localStorage for dark mode setting on page load
+window.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('darkMode') === 'enabled') {
+    enableDarkMode();
+    checkbox.checked = true;
   }
 });
 
-function keepBlack() {
+// Toggle dark mode and store the setting in localStorage
+checkbox.addEventListener('change', () => {
+  if (checkbox.checked) {
+    enableDarkMode();
+    localStorage.setItem('darkMode', 'enabled');
+  } else {
+    disableDarkMode();
+    localStorage.setItem('darkMode', 'disabled');
+  }
+});
+
+function enableDarkMode() {
+  body.style.backgroundColor = 'var(--darkest)';
+  body.style.color = 'white';
+  body.style.transition = '0.2s';
+}
+
+function disableDarkMode() {
+  body.style.backgroundColor = 'var(--lightest)';
+  body.style.color = 'black';
+  body.style.transition = '0.2s';
+}
+
+function keepIconsBlack() {
   let homeIcon = document.querySelector('.fa-user');
   let portfolioIcon = document.querySelector('.fa-folder');
   homeIcon.style.color = 'black';
   portfolioIcon.style.color = 'black';
 }
+keepIconsBlack();
 
 navIcon.addEventListener('click', function () {
   navIcon.classList.toggle('open');
 });
 
 // Developer Tools Menu
-
 console.group(
   `%cHi, I'm Chris Azuaje and thanks for checking out my portfolio!`,
   styles
